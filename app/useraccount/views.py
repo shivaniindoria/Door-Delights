2b1be6f5adb4b11403d3ef6f4c68feb64baf4d59
shivaniindoria <shivani.indoria30@gmail.com>
2022-07-17 01:36:48 +0530

    loginView

--- app/useraccount/views.py
+++ app/useraccount/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+def LoginView(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,'Successfully Logged In')
+            return redirect('/')
+        else:
+            messages.error(request,'invalid username or password!')
+            return redirect('login')
+    return render(request,"useraccount/login.html")
